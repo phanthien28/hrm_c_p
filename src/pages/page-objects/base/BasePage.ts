6418db05c2b8559bcc6a3f5fdd8df8177bc59fff
thien <phanthothien204@gmail.com>
2025-07-04 14:28:19 +0700

    h

--- src/pages/page-objects/base/BasePage.ts
+++ src/pages/page-objects/base/BasePage.ts
@@ -28,7 +28,7 @@ export class BasePage {
     // Validate current URL contains expected path
     async validateUrl(expectedPath: string) {
         const cleanPath = expectedPath.replace(/\*\*/g, '');
-        await this.page.waitForURL(`**${cleanPath}`, { timeout: 20000 });
+        await this.page.waitForURL(`**${cleanPath}`, { timeout: 60000 });
         const currentUrl = this.page.url();
         expect(currentUrl).toContain(cleanPath);
 }
